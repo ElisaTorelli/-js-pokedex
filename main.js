559112abf070b077e0573a3410d7ce70eade033f
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-06 17:00:50 +0200

    add code

--- main.js
+++ main.js
@@ -2,19 +2,20 @@
 let cardContainer = document.querySelector(".main__container");
 
 function displayPokemon() {
-    for(let i = 1; i <= 151; i++){
+    for(let i = 1; i < 151; i++){
         fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
             .then((response) => response.json())
             .then((data) => {
                 (cardContainer.innerHTML += `
-                <div class="pokemonCard">
+                <div class="pokemonCard ${data.types[0].type.name}">
                     <img src="${data.sprites.front_default}" alt="img">
                     <div class="card__info">
                         <span>#${data.id}</span>
                         <h4>${data.name}</h4>
                         <h6>Type: ${data.types[0].type.name}</h6>
                     </div>
-                </div>`)
+                </div>`
+                )
             }
         );
     }
