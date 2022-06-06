3a7d7c7c73a5e209e01cebde22b6b8f258d7f0a4
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-06 16:11:54 +0200

    add code snippet

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let cardContainer = document.querySelector(".main__container");
+
+function displayPokemon() {
+    for(let i = 1; i <= 151; i++){
+        fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+            .then((response) => response.json())
+            .then((data) => {
+                (cardContainer.innerHTML += `
+                <div class="pokemonCard">
+                    <img src="${data.sprites.front_default}" alt="img">
+                    <div class="card__info">
+                        <span>#${data.id}</span>
+                        <h4>${data.name}</h4>
+                        <h6>Type: ${data.types[0].type.name}</h6>
+                    </div>
+                </div>`)
+            }
+        );
+    }
+}
+
+displayPokemon();
